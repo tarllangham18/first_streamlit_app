0af3ed10a0a26d337b7c3eccf25730a4e88da13c
Tarl Langham <105985011+tarllangham18@users.noreply.github.com>
2022-05-21 12:07:07 -0500

    Update streamlit_app.py
    New change for a test

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Blueberyy Meal')
+streamlit.text('🥣 Omega 3 & Bluebery Meal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
