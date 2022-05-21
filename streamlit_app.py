5763c673dd871dcd49b8d002fd9aeda52a80397d
Tarl Langham <105985011+tarllangham18@users.noreply.github.com>
2022-05-21 11:42:43 -0500

    Update streamlit_app.py
    Fixed Spacing

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('🥣gOmega 3 & Blueberyy Meal')
-streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔Hard-Boiled Free Range Egg')
-streamlit.text('🥑🍞Avacado Toast')
+streamlit.text('🥣 Omega 3 & Blueberyy Meal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
