0372075b88b06897d3999c1df1c420087678b32a
Tarl Langham <105985011+tarllangham18@users.noreply.github.com>
2022-05-21 11:41:59 -0500

    Update streamlit_app.py
    Added New Breakfast Menu with pictures

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣gOmega 3 & Blueberyy Meal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free Range Egg')
+streamlit.text('🥑🍞Avacado Toast')
