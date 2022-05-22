f2d3623ddf12f6b4a0814893c06391a59c4ed17a
Tarl Langham <105985011+tarllangham18@users.noreply.github.com>
2022-05-21 19:53:53 -0500

    Update streamlit_app.py
    Added table formatting to the API responce

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,10 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+
+
+# take json version of the response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output it on the screen as a table
+streamlit.dataframe(fruityvice_normalized)
